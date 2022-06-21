7afdda68e074393fac23100cb8b4b6a5223bf844
AyushKatochh <katochayushhp@gmail.com>
2022-06-21 20:14:25 +0530

    Table booking

---
+++ routes/hotels.js
@@ -0,0 +1,26 @@
+import express, { application } from "express";
+import { countByCity, countByType, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+import {verifyAdmin} from "../utils/verifyToken.js"
+
+const router = express.Router();
+
+//CREATE 
+router.post("/", verifyAdmin, createHotel);
+
+
+//UPDATE
+router.put("/:id", verifyAdmin, updateHotel);
+
+//DELETE
+router.delete("/:id", verifyAdmin, deleteHotel);
+//GET
+router.get("/find/:id", getHotel);
+
+// GET ALL
+router.get("/", getHotels);
+router.get("/countByCity", countByCity);
+router.get("/countByType", countByType);
+
+export default router; 
